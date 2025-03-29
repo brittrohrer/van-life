27cf8be9a8c0e9bcf9a860317edc32fbac3fc0f4
brittrohrer <159202820+brittrohrer@users.noreply.github.com>
2025-03-29 14:18:24 -0400

    added startup with minor files

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
